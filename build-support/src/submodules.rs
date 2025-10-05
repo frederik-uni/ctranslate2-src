cbbb77a8237a645ef971c7c29df8bcea8799da3e
frederik <termite_pilgrim04@icloud.com>
2025-10-05 13:37:53 +0200

    .

--- build-support/src/submodules.rs
+++ build-support/src/submodules.rs
@@ -1,6 +1,6 @@
 use base64::Engine;
 use serde::Deserialize;
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
