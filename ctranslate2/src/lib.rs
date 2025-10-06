87b196884d0267b11e7551af4705a336c7459ebc
frederik <termite_pilgrim04@icloud.com>
2025-10-06 16:13:26 +0200

    add docs

--- ctranslate2/src/lib.rs
+++ ctranslate2/src/lib.rs
@@ -1,9 +1,22 @@
+//! # Basic Usage
+//! ```
+//!  let sources = vec![
+//!     "Hallo World!",
+//!     "This crate provides Rust bindings for CTranslate2."
+//! ];
+//! let translator = Translator2::new("/path/to/model", &Default::default(), tokenizer::rust_tokenizers::SentenceTokenizer::new("/path/to/tokenizer"))?;
+//! let results = translator.translate_batch(&sources, &Default::default())?;
+//! for (r, _) in results{
+//!     println!("{}", r);
+//! }
+//! ```
+
+//!
 pub mod compute_type;
 pub mod device;
 pub mod tokenizer;
 pub mod translator;
 pub mod translator2;
-
 pub use compute_type::ComputeType;
 pub use device::Device;
 pub use tokenizer::Tokenizer;
